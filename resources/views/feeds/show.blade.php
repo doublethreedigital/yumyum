573edba9bd2adf1b5953bca7201242fcd6cce9f9
Duncan McClean <duncan@mcclean.co.uk>
2021-02-01 21:21:25 +0000

    show recent runs in cp

--- resources/views/feeds/show.blade.php
+++ resources/views/feeds/show.blade.php
@@ -1,17 +1,36 @@
 @extends('statamic::layout')
-@section('title', 'Feed')
+@section('title', $feed->name())
 @section('wrapper_class', 'max-w-xl')
 
 @section('content')
     <header class="mb-3">
         <div class="flex items-center justify-between">
             <h1>{{ $feed->name() }}</h1>
-            <h3>{{ $feed->source() }}</h3>
             <a href="{{ $feed->editUrl() }}">Edit</a>
         </div>
     </header>
 
-    <p>TODO: error log</p>
-    <p>TODO: show recent runs</p>
-    <p>TODO: show recent saved entries</p>
+    <div class="content">
+        <h2 class="mb-1">Recent Runs</h2>
+    </div>
+
+    <table class="data-table bg-white rounded w-full">
+        <thead>
+            <tr>
+                <th>Date & Time</th>
+                <th>Items saved</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($feed->runs() as $run)
+                <tr>
+                    <td>
+                        <div class="little-dot mr-1 @if($run['result'] == 'success') bg-green @else bg-gray @endif"></div>
+                        <a href="#">{{ \Carbon\Carbon::parse($run['date'])->format('Y-m-d H:i:s') }}</a>
+                    </td>
+                    <td>{{ collect($run['saved'])->count() }} item</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
