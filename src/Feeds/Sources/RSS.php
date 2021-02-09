57d0ef6a1b5b3a0ccc13584b9d335397d8e24cf5
Duncan McClean <duncan@mcclean.co.uk>
2021-02-09 20:16:58 +0000

    Add a code comment here
    Otherwise, it’s gonna confuse the heck out of me one day.

--- src/Feeds/Sources/RSS.php
+++ src/Feeds/Sources/RSS.php
@@ -9,6 +9,7 @@ use SimpleXMLElement;
 class RSS implements Contract
 {
     protected $source;
+    protected $colonSeparator = 'YUMCOOKIETIME';
 
     public function __construct(array $source)
     {
@@ -21,7 +22,12 @@ class RSS implements Contract
 
         try {
             $file = file_get_contents($this->source['url']);
-            $file = preg_replace('~(</?|\s)([a-z0-9_]+):~is', '$1$2YUMCOOKIETIME', $file);
+
+            // When an RSS feed has a `:` in an attribute key, Simple XML will just ignore the
+            // attributes altogether. This regex temporarily swaps it out before we parse the
+            // feed and then it is swapped back in later on when we map it into a Collection.
+            $file = preg_replace('~(</?|\s)([a-z0-9_]+):~is', "$1$2{$this->colonSeparator}", $file);
+
             $feed = simplexml_load_string($file);
         } catch (\Exception $e) {
             throw new \Exception("There was an issue parsing the RSS feed provided. ({$this->source['url']})");
@@ -54,14 +60,14 @@ class RSS implements Contract
         return collect($items)
             ->map(function (SimpleXMLElement $item) {
                 return collect((array) $item)->mapWithKeys(function ($value, $key) {
-                    $key = str_replace('YUMCOOKIETIME', ':', $key);
+                    $key = str_replace($this->colonSeparator, ':', $key);
 
                     if ($value instanceof SimpleXMLElement) {
                         return [$key => (array) $value];
                     }
 
                     if (is_string($value)) {
-                        return [$key => str_replace('YUMCOOKIETIME', ':', $value)];
+                        return [$key => str_replace($this->colonSeparator, ':', $value)];
                     }
 
                     return null;
